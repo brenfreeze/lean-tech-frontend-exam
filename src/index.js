ca44730fc28b022a782b977ce2d98ae7e915b427
brenfreeze <bren24.com@gmail.com>
2020-07-30 19:04:30 +0800

    Added sass loaders

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+import './styles/styles.scss'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      }
     ],
   },
   resolve: {
