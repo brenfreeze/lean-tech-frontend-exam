90a95e3024355688ec636f28d89ac915851be5ff
brenfreeze <bren24.com@gmail.com>
2020-08-03 17:01:45 +0800

    Added Form isSubmitting State

--- src/components/ProgramForm.jsx
+++ src/components/ProgramForm.jsx
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { Formik } from 'formik'
-import { useMutation } from '@apollo/client'
+import React, { useState, useEffect } from "react"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import { Formik } from "formik"
+import { useMutation } from "@apollo/client"
 
-import Modal from './Modal'
-import Input from './Input'
+import Modal from "./Modal"
+import Input from "./Input"
 
-import {
-  ADD_PROGRAM,
-  UPDATE_PROGRAM
-} from '../services/queries'
-import validator from '../services/validator'
-import { openDialog } from '../store/reducers/Utilities/actions'
-import { addProgram as addProgramToStore, } from '../store/reducers/Programs/actions'
+import { ADD_PROGRAM, UPDATE_PROGRAM } from "../services/queries"
+import validator from "../services/validator"
+import { openDialog } from "../store/reducers/Utilities/actions"
+import { addProgram as addProgramToStore } from "../store/reducers/Programs/actions"
 
 const ProgramForm = ({
   addProgramToStore,
@@ -23,10 +20,10 @@ const ProgramForm = ({
   ...props
 }) => {
   const [initialFormValues, setInitialFormValues] = useState({
-    operation: '',
-    processId: '',
+    operation: "",
+    processId: "",
     processStep: 0,
-    productFamily: ''
+    productFamily: "",
   })
   const [addProgram] = useMutation(ADD_PROGRAM)
   const [updateProgram] = useMutation(UPDATE_PROGRAM)
@@ -41,24 +38,23 @@ const ProgramForm = ({
         productFamily: toUpdate.productFamily,
       })
     }
-  }, [ ])
+  }, [])
 
   const addProgramSubmit = (values) => {
     addProgram({
       variables: {
-        ...values
-      }
-    })
-      .then((result) => {
-        const { createProgram } = result.data
+        ...values,
+      },
+    }).then((result) => {
+      const { createProgram } = result.data
 
-        addProgramToStore(createProgram)
-        openDialog({
-          title: `Added ${createProgram.operation}`
-        })
-
-        props.onClose()
+      addProgramToStore(createProgram)
+      openDialog({
+        title: `Added ${createProgram.operation}`,
       })
+
+      props.onClose()
+    })
   }
 
   const updateProgramSubmit = (values) => {
@@ -66,26 +62,25 @@ const ProgramForm = ({
       variables: {
         id: toUpdate.id,
         program: {
-          ...values
-        }
-      }
-    })
-      .then((result) => {
-        const { updateProgram } = result.data
+          ...values,
+        },
+      },
+    }).then((result) => {
+      const { updateProgram } = result.data
 
-        /*
+      /*
           Would add an updateProgramFromStore function here,
           but apollo-client already handles the state update.
         */
-        openDialog({
-          title: `Updated ${updateProgram.operation}`
-        })
-
-        props.onClose()
+      openDialog({
+        title: `Updated ${updateProgram.operation}`,
       })
+
+      props.onClose()
+    })
   }
 
-  const onProgramFormSubmit = values => {
+  const onProgramFormSubmit = (values) => {
     if (isUpdating) {
       updateProgramSubmit(values)
 
@@ -96,9 +91,9 @@ const ProgramForm = ({
   }
 
   return (
-    <Modal { ...props }>
+    <Modal {...props}>
       <h1 className="title">
-        { isUpdating ? `Update ${toUpdate.operation}` : 'Add New Program'}
+        {isUpdating ? `Update ${toUpdate.operation}` : "Add New Program"}
       </h1>
       <Formik
         enableReinitialize
@@ -106,63 +101,74 @@ const ProgramForm = ({
         onSubmit={onProgramFormSubmit}
         validate={validator}
       >
-        {({ errors, touched, values, handleChange, handleSubmit, }) => (
-            <form
-              className="program-form"
-              onSubmit={handleSubmit}
-              autoComplete="off"
-            >
-              <Input
-                type="text"
-                id="operation"
-                name="operation"
-                label="Operation *"
-                isError={touched.operation && errors.operation}
-                onChange={handleChange}
-                value={values.operation}
-              />
-              <Input
-                type="text"
-                id="processId"
-                name="processId"
-                label="Process ID *"
-                isError={touched.processId && errors.processId}
-                onChange={handleChange}
-                value={values.processId}
-              />
-              <Input
-                type="number"
-                id="processStep"
-                name="processStep"
-                label="Process Step *"
-                isError={touched.processStep && errors.processStep}
-                onChange={handleChange}
-                value={values.processStep}
-              />
-              <Input
-                type="text"
-                id="productFamily"
-                name="productFamily"
-                label="Product Family *"
-                isError={touched.productFamily && errors.productFamily}
-                onChange={handleChange}
-                value={values.productFamily}
-              />
-              <button type="submit" className="btn">
-                Save
-              </button>
-            </form>
-          )}
+        {({
+          errors,
+          touched,
+          values,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form
+            className="program-form"
+            onSubmit={handleSubmit}
+            autoComplete="off"
+          >
+            <Input
+              type="text"
+              id="operation"
+              name="operation"
+              label="Operation *"
+              isError={touched.operation && errors.operation}
+              onChange={handleChange}
+              value={values.operation}
+            />
+            <Input
+              type="text"
+              id="processId"
+              name="processId"
+              label="Process ID *"
+              isError={touched.processId && errors.processId}
+              onChange={handleChange}
+              value={values.processId}
+            />
+            <Input
+              type="number"
+              id="processStep"
+              name="processStep"
+              label="Process Step *"
+              isError={touched.processStep && errors.processStep}
+              onChange={handleChange}
+              value={values.processStep}
+            />
+            <Input
+              type="text"
+              id="productFamily"
+              name="productFamily"
+              label="Product Family *"
+              isError={touched.productFamily && errors.productFamily}
+              onChange={handleChange}
+              value={values.productFamily}
+            />
+            <button type="submit" className="btn" disabled={isSubmitting} style={{ marginBottom: '0.5rem' }}>
+              {isSubmitting ? "..." : "Save"}
+            </button>
+            <button className="btn outline" onClick={props.onClose} disabled={isSubmitting}>Cancel</button>
+          </form>
+        )}
       </Formik>
     </Modal>
   )
 }
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-    openDialog,
-    addProgramToStore,
-  }, dispatch)
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      openDialog,
+      addProgramToStore,
+    },
+    dispatch
+  )
 }
 
 export default connect(null, mapDispatchToProps)(ProgramForm)
